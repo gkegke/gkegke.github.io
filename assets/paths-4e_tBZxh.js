e8f309812f502819516403a131d31572c00e965a
gkegke <gdxwillcome@gmail.com>
2026-02-03 14:10:36 +0000

    Updates

--- assets/paths-4e_tBZxh.js
+++
@@ -1,1 +0,0 @@
-const s={POSTS_MARKDOWN:"/posts/",PROJECT_IMAGES:"/project_imgs/"};export{s as P};
---
+++ assets/paths-B7jSldaf.js
@@ -0,0 +1,1 @@
+const S={POSTS_MARKDOWN:"./posts/",PROJECT_IMAGES:"./project_imgs/"};export{S as P};
